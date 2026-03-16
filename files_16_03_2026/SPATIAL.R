149f65a9cfa43c24a168bf9f13ea907caeb0a1a3
felixpetschko <felixpetschko@gmail.com>
2026-03-16 18:45:34 +0100

    precompute images

--- files_16_03_2026/SPATIAL.R
+++ files_16_03_2026/SPATIAL.R
@@ -8,32 +8,49 @@ library(presto)
 library(spacedeconv)
 library(SpatialExperiment)
 
+plot_dir <- "spatial_plots"
+dir.create(plot_dir, showWarnings = FALSE, recursive = TRUE)
+
+save_plot <- function(plot_obj, filename, width = 8, height = 6, dpi = 300) {
+  ggplot2::ggsave(
+    filename = file.path(plot_dir, filename),
+    plot = plot_obj,
+    width = width,
+    height = height,
+    dpi = dpi
+  )
+}
+
 brca_sp <- readRDS("../Data/brca_spatial_s4.rds")
 print(brca_sp)
 
-SpatialPlot(brca_sp, alpha = 0)
+p <- SpatialPlot(brca_sp, alpha = 0)
+save_plot(p, "01_spatial_plot.png")
 
 head(brca_sp@meta.data)
 
-VlnPlot(
+p <- VlnPlot(
   brca_sp,
   features = c("nCount_Spatial", "nFeature_Spatial"),
   pt.size = 0.1
 ) + NoLegend()
+save_plot(p, "02_qc_violin.png")
 
-SpatialFeaturePlot(
+p <- SpatialFeaturePlot(
   brca_sp,
   features = "nCount_Spatial",
   image.alpha = 0
 ) +
   theme(legend.position = "right")
+save_plot(p, "03_ncount_spatial.png")
 
-SpatialFeaturePlot(
+p <- SpatialFeaturePlot(
   brca_sp,
   features = "nCount_Spatial",
   image.alpha = 0
 ) +
   theme(legend.position = "right")
+save_plot(p, "04_nfeature_spatial_placeholder.png")
 
 brca_sp <- SCTransform(
   brca_sp,
@@ -41,13 +58,17 @@ brca_sp <- SCTransform(
   verbose = FALSE
 )
 
-SpatialFeaturePlot(brca_sp, features = c("KRT8", "COL1A2", "CD8B"))
+p <- SpatialFeaturePlot(brca_sp, features = c("KRT8", "COL1A2", "CD8B"))
+save_plot(p, "05_marker_triplet.png", width = 10, height = 4)
 
-SpatialFeaturePlot(brca_sp, features = "KRT8")
+p <- SpatialFeaturePlot(brca_sp, features = "KRT8")
+save_plot(p, "06_krt8_default.png")
 
-SpatialFeaturePlot(brca_sp, features = "KRT8", pt.size.factor = 1)
+p <- SpatialFeaturePlot(brca_sp, features = "KRT8", pt.size.factor = 1)
+save_plot(p, "07_krt8_ptsize1.png")
 
-SpatialFeaturePlot(brca_sp, features = "KRT8", alpha = c(0.1, 1))
+p <- SpatialFeaturePlot(brca_sp, features = "KRT8", alpha = c(0.1, 1))
+save_plot(p, "08_krt8_alpha.png")
 
 brca_sp <- RunPCA(
   brca_sp,
@@ -70,29 +91,33 @@ brca_sp <- RunUMAP(
   dims = 1:30
 )
 
-DimPlot(
+p <- DimPlot(
   brca_sp,
   reduction = "umap",
   label = TRUE
 )
+save_plot(p, "09_umap_clusters.png")
 
-SpatialDimPlot(brca_sp, label = TRUE, label.size = 3)
+p <- SpatialDimPlot(brca_sp, label = TRUE, label.size = 3)
+save_plot(p, "10_spatial_clusters.png")
 
-SpatialDimPlot(
+p <- SpatialDimPlot(
   brca_sp,
   cells.highlight = CellsByIdentities(object = brca_sp, idents = c(1, 4)),
   facet.highlight = TRUE,
   ncol = 2
 )
+save_plot(p, "11_spatial_highlight_clusters.png", width = 10, height = 5)
 
 de_markers <- FindMarkers(brca_sp, ident.1 = 1, ident.2 = 4)
 head(de_markers)
-SpatialFeaturePlot(
+p <- SpatialFeaturePlot(
   object = brca_sp,
   features = rownames(de_markers)[1:4],
   alpha = c(0.1, 1),
   ncol = 2
 )
+save_plot(p, "12_de_markers_top4.png", width = 10, height = 8)
 
 brca_sc <- readRDS("../Data/brca_sc_s4.rds")
 
@@ -155,7 +180,7 @@ brca_sp <- TransferData(
 
 DefaultAssay(brca_sp) <- "prediction.score.id"
 
-SpatialFeaturePlot(
+p <- SpatialFeaturePlot(
   brca_sp,
   features = c(
     "Cancer Epithelial",
@@ -167,6 +192,7 @@ SpatialFeaturePlot(
   ),
   ncol = 2
 )
+save_plot(p, "13_label_transfer_scores.png", width = 10, height = 8)
 
 brca_sce <- as.SingleCellExperiment(brca_sc)
 
@@ -190,7 +216,7 @@ brca_sdwls_res <- spacedeconv::deconvolute(
   signature = brca_sdwls_sig
 )
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "spatialdwls_CAFs",
   sample_id = "brca",
@@ -198,8 +224,9 @@ plot_spatial(
   title = "CAF",
   density = FALSE
 )
+save_plot(p, "14_spatialdwls_cafs.png")
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "spatialdwls_CAFs",
   sample_id = "brca",
@@ -208,8 +235,9 @@ plot_spatial(
   smooth = TRUE,
   density = FALSE
 )
+save_plot(p, "15_spatialdwls_cafs_smooth.png")
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "spatialdwls_Myeloid",
   sample_id = "brca",
@@ -218,8 +246,9 @@ plot_spatial(
   smooth = TRUE,
   density = FALSE
 )
+save_plot(p, "16_spatialdwls_myeloid_smooth.png")
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "spatialdwls_T.cells",
   sample_id = "brca",
@@ -228,8 +257,9 @@ plot_spatial(
   smooth = TRUE,
   density = FALSE
 )
+save_plot(p, "17_spatialdwls_tcells_smooth.png")
 
-spacedeconv::plot_comparison(
+p <- spacedeconv::plot_comparison(
   brca_sdwls_res,
   cell_type_1 = "spatialdwls_Myeloid",
   cell_type_2 = "spatialdwls_CAFs",
@@ -243,8 +273,9 @@ spacedeconv::plot_comparison(
   shift_positive = FALSE,
   nDigits = 2
 )
+save_plot(p, "18_myeloid_vs_cafs.png")
 
-plot_most_abundant(
+p <- plot_most_abundant(
   brca_sdwls_res,
   method = "spatialdwls",
   sample_id = "brca",
@@ -253,6 +284,7 @@ plot_most_abundant(
   min_abundance = 0.05,
   palette = "Accent"
 )
+save_plot(p, "19_most_abundant.png")
 
 tf_ref <- get_decoupleR_reference(
   method = "collectri",
@@ -276,7 +308,7 @@ brca_sdwls_res <- compute_activities(
   assay = "cpm"
 )
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "progeny_JAK.STAT",
   sample_id = "brca",
@@ -285,8 +317,9 @@ plot_spatial(
   smooth = TRUE,
   density = FALSE
 )
+save_plot(p, "20_progeny_jak_stat.png")
 
-plot_spatial(
+p <- plot_spatial(
   spe = brca_sdwls_res,
   result = "progeny_TGFb",
   sample_id = "brca",
@@ -295,3 +328,4 @@ plot_spatial(
   smooth = TRUE,
   density = FALSE
 )
+save_plot(p, "21_progeny_tgfb.png")
